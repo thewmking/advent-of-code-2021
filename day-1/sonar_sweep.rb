3600b97de7af4b060dadaf61135c95ca49f6b7f8
thewmking <thewmking@gmail.com>
2021-12-02 16:47:21 -0500

    Refactor increased_indices & add part 2 solution

--- day-1/sonar_sweep.rb
+++ day-1/sonar_sweep.rb
@@ -2001,11 +2001,35 @@ values = %w(182
 
 values.map! { |v| v.to_i }
 
-increased_indices = []
+def measure_increases(values)
+  increased_indices = []
 
-values.each_with_index do |v, i|
-  next if i == 0
-  increased_indices << i if (values[i] > values[i-1])
+  values.each_with_index do |v, i|
+    next if i == 0
+    increased_indices << i if (values[i] > values[i-1])
+  end
+
+  increased_indices
 end
 
-puts increased_indices.length # => 1215
+
+# part 1
+# How many measurements are larger than the previous measurement?
+measurements = measure_increases(values)
+puts measurements.length # => 1215
+
+
+# part 2
+# Consider sums of a three-measurement sliding window.
+# How many sums are larger than the previous sum?
+
+sums = []
+values.each_with_index do |v, i|
+  # grab the sums of each set of 3
+  if !values[i+2].nil?
+    sums << (values[i] + values[i+1] + values[i+2])
+  end
+end
+
+increased_sums = measure_increases(sums)
+puts increased_sums.length # => 1150
