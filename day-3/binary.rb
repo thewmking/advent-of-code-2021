56a3406ca29f0421cd92e1ad14ca8fae7c66171a
thewmking <thewmking@gmail.com>
2021-12-03 22:09:19 -0500

    Refactor day 3 part one

--- day-3/binary.rb
+++ day-3/binary.rb
@@ -1,7 +1,7 @@
 # --- Day 3: Binary Diagnostic ---
 
 file = File.open('day-3/input.txt')
-file_data = file.readlines.map(&:chomp)
+@file_data = file.readlines.map(&:chomp)
 file.close
 
 # You need to use the binary numbers in the diagnostic report to generate two new binary numbers (called the gamma rate and the epsilon rate).
@@ -11,31 +11,43 @@ file.close
 # The epsilon rate is calculated in a similar way; rather than use the most common bit, the least common bit from each position is used.
 @gamma, @epsilon = [], []
 
-def binary_to_float(binary)
+def binary_to_decimal(binary)
   binary.to_i(2)
 end
 
-data_by_column = []
 
-# group array data by index into new object
-file_data.each do |row|
-  row.split('').each_with_index do |val, i|
-    if data_by_column[i].nil?
-      data_by_column[i] = [val]
-    else
-      data_by_column[i] << val
+def data_by_column(data)
+  data_by_column = []
+
+  # group array data by index into new object
+  data.each do |row|
+    row.split('').each_with_index do |val, i|
+      if data_by_column[i].nil?
+        data_by_column[i] = [val]
+      else
+        data_by_column[i] << val
+      end
     end
   end
+
+  data_by_column
 end
 
-data_by_column.each do |values|
-  # gets min and max at once
-  e, g = values.minmax_by{ |i| values.count(i) }
+def min_max(values)
+  values.minmax_by{ |v| values.count(v) }
+end
+
+
+data_by_column(@file_data).each do |values|
+  e, g = min_max(values)
   @gamma << g
   @epsilon << e
 end
 
-g_float = binary_to_float(@gamma.join)
-e_float = binary_to_float(@epsilon.join)
+g_dec = binary_to_decimal(@gamma.join)
+e_dec = binary_to_decimal(@epsilon.join)
+
+puts g_dec * e_dec # => 4160394
+
 
 puts g_float * e_float # => 4160394
